e078e299ae89bb50460c7697abf88d1162092bdb
Chetan Sanghani <crsanghani@gmail.com>
2016-09-23 15:21:33 +0100

    amdended my method to pass the third test

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,15 @@ class Fizzbuzz
 
   def number(int)
     int == 3 ? "Fizz" : "Buzz"
+
+    if int == 3
+      return "Fizz"
+    elsif int == 5
+      return "Buzz"
+    else
+      return "FizzBuzz"
+    end
+
   end
 
 end
